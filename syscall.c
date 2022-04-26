b762da20ed9db03049445f862af71fbad3c4b0fc
Ayler <liyale@bixin.cn>
2022-04-26 19:00:59 +0800

    fetch str

--- syscall.c
+++ syscall.c
@@ -18,11 +18,7 @@ int
 fetchint(uint addr, int *ip)
 {
   struct proc *curproc = myproc();
-  // cprintf("stack size: %d\n", curproc->stacksz);
-  // cprintf("addr is %d at %p\n", *(int*)(addr), addr);
-  // cprintf("ip is %d at %p\n", *ip, ip);
-  // *ip = *(int*)(addr);
-  // return 0;
+
   // if(addr >= curproc->sz || addr+4 > curproc->sz)
   //   return -1;
   if(curproc->stacksz != 0 && (addr >= curproc->stacksz || addr+4 > curproc->stacksz))
@@ -39,13 +35,16 @@ fetchstr(uint addr, char **pp)
 {
   char *s, *ep;
   struct proc *curproc = myproc();
-  cprintf("fetchstr fault");
+  cprintf("stack size: %d\n", curproc->stacksz);
+  cprintf("addr is %d at %p\n", *(int*)(addr), addr);
+  cprintf("pp is %d at %p\n", *pp, pp);
+  // *ip = *(int*)(addr);
   return -1;
 
-  if(addr >= curproc->sz)
+  if(addr >= curproc->stacksz)
     return -1;
   *pp = (char*)addr;
-  ep = (char*)curproc->sz;
+  ep = (char*)curproc->stacksz;
   for(s = *pp; s < ep; s++){
     if(*s == 0)
       return s - *pp;
